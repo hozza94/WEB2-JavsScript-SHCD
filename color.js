3bac60a0f7d7f0055e3b56594078538f3c99667c
hozza94 <hozza94@users.noreply.github.com>
2021-04-27 19:27:02 +0900

    update
    jquery

---
+++ color.js
@@ -0,0 +1,42 @@
+var Body = {
+  setColor:function (color) {
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color) {
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+
+var Links = {
+  setColor:function (color) {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+
+    while ( i < alist.length) {
+     alist[i].style.color = color;
+     i = i + 1;
+     }
+  }
+
+}
+
+
+function NightDayHandler(self){
+  var target = document.querySelector('body');
+
+  if (self.value === 'LIGHT OFF') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'LIGHT ON'
+
+    Links.setColor('powderblue')
+
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'LIGHT OFF'
+
+    Links.setColor('blue')
+
+  }
+}
